55bada5bde42c8d50efd1e428e766c83ccb2e416
Stiv1100001 <bichicchi.stefano@gmail.com>
2022-02-24 14:10:49 +0100

    Add some magic

--- js/main.js
+++ js/main.js
@@ -9,19 +9,20 @@ for (let i = 1; i <= 100; i++) {
     'justify-content-center',
     'align-items-center'
   );
+  newDiv.style.animationDelay = Math.random() * 10 + 1 + 's';
 
   if (i % 3 === 0 && i % 5 === 0) {
     newDiv.innerHTML = '<h5 class="fw-bold"> FizzBuzz</h5 > ';
-    newDiv.classList.add('bg-primary');
+    newDiv.classList.add('bg-primary', 'flip');
   } else if (i % 3 === 0) {
     newDiv.innerHTML = '<h5 class="fst-italic">Fizz</h5>';
-    newDiv.classList.add('bg-danger');
+    newDiv.classList.add('bg-danger', 'rotate');
   } else if (i % 5 === 0) {
     newDiv.innerHTML = '<h5 class="text-decoration-underline">Buzz</h5>';
-    newDiv.classList.add('bg-warning');
+    newDiv.classList.add('bg-warning', 'shadow-pop');
   } else {
     newDiv.innerHTML = '<h5>' + i + '</h5>';
-    newDiv.classList.add('bg-white');
+    newDiv.classList.add('bg-white', 'bg-transform');
   }
 
   content.appendChild(newDiv);
